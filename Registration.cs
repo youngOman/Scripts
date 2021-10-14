5f991f11098e3a8268d788af14d12f30d45dd8e5
young <young@butyshop.com>
2021-10-14 10:48:42 +0800

    MysqlUpload Data

--- Registration.cs
+++ Registration.cs
@@ -14,31 +14,32 @@ public class Registration : MonoBehaviour
     public Text ErrorText;
     public void CallRegister(){
         Debug.Log("RUNNNNNNNNNNNNNNNNNNNN");
-        StartCoroutine(Register("http://localhost/UnitySqlconnect/register.php", result =>{Debug.Log(result);}));
+        // StartCoroutine(Register("http://localhost/UnitySqlconnect/register.php", result =>{Debug.Log(result);}));
+        StartCoroutine(Register("test","123456","123"));
+
     }   
-    IEnumerator Register(string url, Action<string> result){
-    UnityWebRequest www = UnityWebRequest.Get(url);
-    yield return www.SendWebRequest();
-    if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-            if (result != null)
-                result(www.error);
-        }
-        else
-        {
-            Debug.Log(www.downloadHandler.data); // this log is returning the requested data. 
-            if (result != null)
-                result(www.downloadHandler.text);
-        }
-    }
-    IEnumerator login(string InputplayerID,string InputPasswd,string InputPlayerName)
+    // IEnumerator Register(string url, Action<string> result){
+    // UnityWebRequest www = UnityWebRequest.Get(url);
+    // yield return www.SendWebRequest();
+    // if (www.isNetworkError || www.isHttpError)
+    //     {
+    //         Debug.Log(www.error);
+    //         if (result != null)
+    //             result(www.error);
+    //     }
+    //     else
+    //     {
+    //         Debug.Log(www.downloadHandler.data); // this log is returning the requested data. 
+    //         if (result != null)
+    //             result(www.downloadHandler.text);
+    //     }
+    // }
+    IEnumerator Register(string InputplayerID,string InputPasswd,string InputPlayerName)
     {
         WWWForm form = new WWWForm();
         form.AddField("InputplayerID",InputplayerID);
         form.AddField("InputPasswd",InputPasswd);
         form.AddField("InputPlayerName",InputPlayerName);
-
         using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/UnitySqlconnect/register.php", form))
         {
             yield return www.SendWebRequest();
