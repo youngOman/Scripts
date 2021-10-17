42df65682ed9a03441833ce6b1f4f54a0832ba9c
young <young@butyshop.com>
2021-10-18 01:57:19 +0800

    2021/10/18 Login_done

--- Login.cs
+++ Login.cs
@@ -1,11 +1,54 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking; // WebRequest需要 
 using UnityEngine.SceneManagement;
-
 public class Login : MonoBehaviour
 {
-    public void LoginToMenu(){
-      SceneManager.LoadScene("MainMenu"); //register index=0
-  }
+    public string url="http://localhost/UnitySqlconnect/login.php";
+    public InputField InputplayerID;
+    public InputField InputPasswd;
+    public Button loginbtn;
+    public Text warningText;
+    public void Call_login(){
+        Debug.Log("RUNNNNNN");
+        string username=InputplayerID.text;
+        string password=InputPasswd.text;
+        StartCoroutine(login(username,password));
+    }   
+    IEnumerator login(string username,string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("InputplayerID",username);
+        form.AddField("InputPasswd",password);
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            www.downloadHandler=new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError){
+                Debug.Log(www.error);
+            }else{  
+                Debug.Log("連線Mysql成功");
+                string responseText=www.downloadHandler.text;
+                if(responseText=="1"){
+                    // PlayerPrefs.SetString("accountusername",username);
+                    SceneManager.LoadScene("MainMenu");
+                }else{
+                    warningText.text="登入失敗!";
+                }
+                warningText.text=responseText;
+                // Debug.Log(responseText);
+                // warning.gameObject.SetActive(true);
+            }   
+        }
+    }
+    public void VerifyInputs(){ //檢查帳密格式
+        loginbtn.interactable=(InputplayerID.text.Length>=8&&InputPasswd.text.Length>=8);
+        // if(string.IsNullOrEmpty(InputplayerID.text))
+    }
+    public void LoadtoRegister(){
+      SceneManager.LoadScene("register");
+    }
 }
--- Registration.cs
+++ Registration.cs
@@ -7,40 +7,25 @@ using UnityEngine.Networking; // WebRequest需要
 using UnityEngine.SceneManagement;
 public class Registration : MonoBehaviour
 {
+    public string url="http://localhost/UnitySqlconnect/register.php";
     public InputField InputplayerID;
     public InputField InputPasswd;
     public InputField InputPlayerName;
     public Button Registerbtn;
-    public Text ErrorText;
+    public Text Warningtext;
     public void CallRegister(){
-        Debug.Log("RUNNNNNNNNNNNNNNNNNNNN");
-        // StartCoroutine(Register("http://localhost/UnitySqlconnect/register.php", result =>{Debug.Log(result);}));
-        StartCoroutine(Register("test","123456","123"));
+        Debug.Log("Runnnn");
+        StartCoroutine(Register());
+        // StartCoroutine(Register("Test","123456","123"));
 
     }   
-    // IEnumerator Register(string url, Action<string> result){
-    // UnityWebRequest www = UnityWebRequest.Get(url);
-    // yield return www.SendWebRequest();
-    // if (www.isNetworkError || www.isHttpError)
-    //     {
-    //         Debug.Log(www.error);
-    //         if (result != null)
-    //             result(www.error);
-    //     }
-    //     else
-    //     {
-    //         Debug.Log(www.downloadHandler.data); // this log is returning the requested data. 
-    //         if (result != null)
-    //             result(www.downloadHandler.text);
-    //     }
-    // }
-    IEnumerator Register(string InputplayerID,string InputPasswd,string InputPlayerName)
+    IEnumerator Register()
     {
         WWWForm form = new WWWForm();
-        form.AddField("InputplayerID",InputplayerID);
-        form.AddField("InputPasswd",InputPasswd);
-        form.AddField("InputPlayerName",InputPlayerName);
-        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/UnitySqlconnect/register.php", form))
+        form.AddField("InputplayerID",InputplayerID.text);
+        form.AddField("InputPasswd",InputPasswd.text);
+        form.AddField("InputPlayerName",InputPlayerName.text);
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
             yield return www.SendWebRequest();
 
@@ -50,12 +35,12 @@ public class Registration : MonoBehaviour
             }
             else
             {
-                Debug.Log("Form upload complete!");
+                Debug.Log("連線成功");
+                SceneManager.LoadScene("login");
             }
         }
     }
     public void VerifyInputs(){ //檢查帳密格式
         Registerbtn.interactable=(InputplayerID.text.Length>=8&&InputPasswd.text.Length>=8);
     }
-    
 }
